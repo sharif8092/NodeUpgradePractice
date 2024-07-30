ca3b21a27363ae77edb874b206c7594697b6c7a5
sharif <sharifkamran8092@gmail.com>
2024-07-30 21:43:37 +0530

    first

---
+++ day1.js/Math.js
@@ -0,0 +1,8 @@
+module.exports.addMath =function Add (a,b){
+    return a+b ;
+}
+
+module.exports.subMath = function Substract (a ,b ){
+    return b -a ;
+}
+
---
+++ day1.js/index.js
@@ -0,0 +1,9 @@
+// import * as Math from "./Math.js"  
+const Math = require ("./Math.js")
+
+
+// console.log("hello world");
+
+// console.log(process.argv.slice(2));
+
+console.log(Math.addMath(2,4));
